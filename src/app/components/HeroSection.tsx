394bb76d2157a5af77a5455ce1b44d530bae4b95
NaziaImran786 <naziaimran.4df@gmail.com>
2025-03-14 01:13:09 +0500

    new update

--- src/app/components/HeroSection.tsx
+++ src/app/components/HeroSection.tsx
@@ -7,7 +7,7 @@ import Link from "next/link";
 const HeroSection = () => {
   return (
     <>
-      <section className="lg:py-16 px-4">
+      <section className="lg:py-16 px-6">
         <div className="grid grid-cols-1 sm:grid-cols-12">
           <div className="col-span-8 place-self-center text-center sm:text-left justify-self-start">
             <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-7xl lg:leading-normal font-extrabold">
@@ -52,9 +52,9 @@ const HeroSection = () => {
           <div className="col-span-4 place-self-center mt-4 lg:mt-0">
             <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
               <Image
-                src={"/images/hero-image.png"}
+                src={"/nazi.png"}
                 alt="hero image"
-                className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+                className="absolute rounded-full transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
                 width={300}
                 height={300}
               />
