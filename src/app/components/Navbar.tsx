8007ece6938fa20b14104929dc53c7057ab401b5
NaziaImran786 <naziaimran.4df@gmail.com>
2025-03-19 01:44:13 +0500

    push

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -31,7 +31,7 @@ const Navbar = () => {
         {
           !navbarOpen ? (
             <button onClick={() => setNavbarOpen(true)} className='flex items-center px-3 py-2 border rounded border-slate-200 hover:text-white hover:border-white'>
-              <Bars3Icon className="h-5 w-5" /> 
+              <Bars3Icon className="h-5 w-5 text-white" /> 
             </button>
           ) : (
             <button  onClick={() => setNavbarOpen(false)} className='flex items-center px-3 py-2 border rounded border-slate-200 hover:text-white hover:border-white'>
